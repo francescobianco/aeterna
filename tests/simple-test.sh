cb6c4ff9e06795adf6aab1f43285be95b969d036
Francesco Bianco <info.francescobianco@gmail.com>
2025-12-25 00:23:18 +0100

    fix

--- tests/simple-test.sh
+++ tests/simple-test.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 set -e
 
-NODE_URL="http://localhost:9915/"
-USER="aeterna"
-PASS="Secret1234!"
-LOCAL_FILE="tests/fixtures/file1.txt"
+NODE1="http://localhost:9915/"
+USER1="aeterna"
+PASS1="Secret1234!"
 
+NODE2="http://localhost:9925/"
+USER2="aeterna"
+PASS2="Secret1234!"
+
+LOCAL_FILE="tests/fixtures/file1.txt"
 touch $LOCAL_FILE
 
-curl -s -u "$USER:$PASS" "$NODE_URL" >/dev/null
+curl -s -u "$USER1:$PASS1" "$NODE1" >/dev/null
+curl -s -u "$USER2:$PASS2" "$NODE2" >/dev/null
+
+curl -s -u "$USER1:$PASS1" "$NODE1/.ssh/id_rsa.pub" -o tests/nodes/pubkey1
 
 curl -s -T "$LOCAL_FILE" -u "$USER:$PASS" "$NODE_URL" >/dev/null
 

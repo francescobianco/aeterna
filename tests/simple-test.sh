edbb2c7e5c17f719b3ca3f0bdb415841e971ea12
Francesco Bianco <info.francescobianco@gmail.com>
2025-12-24 23:49:46 +0100

    fix

--- tests/simple-test.sh
+++ tests/simple-test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -e
 
 NODE_URL="http://localhost:9915/"
 USER="aeterna"
 PASS="Secret1234!"
 LOCAL_FILE="tests/fixtures/file1.txt"
 
+touch $LOCAL_FILE
 
 curl -T "$LOCAL_FILE" -u "$USER:$PASS" "$NODE_URL"
 
